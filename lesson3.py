1df2cd21edcda019d55cde537902cf8cf6017bd5
Mariearakel <mari.arakelyan.1994@gmail.coml>
2023-03-08 20:04:10 +0400

    lesson3.py

---
+++ lesson3.py
@@ -0,0 +1,4 @@
+print("what is git and github")
+print(" how to register in github")
+print("how to add repository in github and how to do clone ")
+print("how to move files from termilav to github ")
