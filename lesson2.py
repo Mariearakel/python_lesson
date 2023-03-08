091908848428750d4b9e6d7385a1f8921a759cef
Mariearakel <mari.arakelyan.1994@gmail.coml>
2023-03-08 19:55:28 +0400

    added lesson.2py file

--- lesson2.py
+++ lesson2.py
@@ -1,3 +1,6 @@
-print("what is git")
-print("what is github")
-print("how to gteatgithub repo")
+print("what kind of extencions have vscode")
+print("haw instal Extencions")
+print("how to write text in terminal")
+print("how python interpreter work")
+print("interpeter vs compilers")
+
