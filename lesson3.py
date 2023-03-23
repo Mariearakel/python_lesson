4a2676e2e3711c7517c97a70452511df43395b6b
Mariearakel <mari.arakelyan.1994@gmail.coml>
2023-03-24 02:27:08 +0400

    added file lesson3.py

--- lesson3.py
+++
@@ -1,4 +0,0 @@
-print("what is git and github")
-print(" how to register in github")
-print("how to add repository in github and how to do clone ")
-print("how to move files from termilav to github ")
